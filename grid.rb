9b75bcbcd0787829f0c7145828ba6c636c3cd63a
macGH <just_mac@hotmail.com>
2016-02-08 17:03:22 -0500

    Grid almost displays now for real.  need to find way to remove trailing space.

--- grid.rb
+++ grid.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Grid
   def initialize
-    @grid = Array.new()
+    @grid = Array.new(10) {Array.new(10) {" "} }
     @ships = Array.new()
   end
 
@@ -17,25 +17,43 @@ class Grid
   end
 
   def display
+    i = 0
     puts "    1   2   3   4   5   6   7   8   9   10"
     puts "  -----------------------------------------"
-    puts "A |   |   |   |   |   |   |   |   |   |   |"
-    puts "B |   |   |   |   |   |   |   |   |   |   |"
-    puts "C |   |   |   |   |   |   |   |   |   |   |"
-    puts "D |   |   |   |   |   |   |   |   |   |   |"
-    puts "E |   |   |   |   |   |   |   |   |   |   |"
-    puts "F |   |   |   |   |   |   |   |   |   |   |"
-    puts "G |   |   |   |   |   |   |   |   |   |   |"
-    puts "H |   |   |   |   |   |   |   |   |   |   |"
-    puts "I |   |   |   |   |   |   |   |   |   |   |"
-    puts "J |   |   |   |   |   |   |   |   |   |   |"
+    @grid.each do |row|
+      print "#{('A'..'Z').to_a[i]} | "
+      row.each do |cell|
+        print cell + " | "
+      end
+      print "\n"
+      i += 1
+    end
     puts "  -----------------------------------------"
+    # puts "A |   |   |   |   |   |   |   |   |   |   |"
+    # puts "B |   |   |   |   |   |   |   |   |   |   |"
+    # puts "C |   |   |   |   |   |   |   |   |   |   |"
+    # puts "D |   |   |   |   |   |   |   |   |   |   |"
+    # puts "E |   |   |   |   |   |   |   |   |   |   |"
+    # puts "F |   |   |   |   |   |   |   |   |   |   |"
+    # puts "G |   |   |   |   |   |   |   |   |   |   |"
+    # puts "H |   |   |   |   |   |   |   |   |   |   |"
+    # puts "I |   |   |   |   |   |   |   |   |   |   |"
+    # puts "J |   |   |   |   |   |   |   |   |   |   |"
   end
 
   def place_ship(ship, col, row, direction)
     ship.place(col, row, direction)
     @ships.each do |i|
       return false if i.overlaps_with?(ship)
+    end
+    if direction
+      (0..ship.length - 1).each do |i|
+        @grid[row - 1][col + i - 1] = "0"
+      end
+    else
+      (0..ship.length - 1).each do |i|
+        @grid[row  + i - 1][col - 1] = "0"
+      end
     end
     @ships << ship
   end
