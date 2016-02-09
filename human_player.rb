5cad338b3811e6474003f9591aa053e22003bea7
macGH <just_mac@hotmail.com>
2016-02-09 15:13:02 -0500

    Test 31 human player can't overlap

--- human_player.rb
+++ human_player.rb
@@ -14,14 +14,22 @@ class HumanPlayer < Player
 
   def place_ships(ship_lengths)
     ship_lengths.each do |l|
-      puts "#{@name}, where would you like to place a ship of length #{l}?"
-      coord = get_user_input
-      puts "Across or Down?"
-      direction = get_user_input
-
-      @ships << Ship.new(l)
-      @grid.place_ship(@ships.last, @grid.x_of(coord), @grid.y_of(coord), direction=="Across" ? true : false)
+      continue = false
+      while !continue
+        puts "#{@name}, where would you like to place a ship of length #{l}?"
+        coord = get_user_input
+        puts "Across or Down?"
+        direction = get_user_input
+        ship = Ship.new(l)
+        continue = @grid.place_ship(ship, @grid.x_of(coord), @grid.y_of(coord), direction=="Across" ? true : false)
+        if !continue
+          puts "Unfortunately, that ship overlaps with one of your other ships.  Please try again."
+        else
+          @ships << ship
+        end
+      end
     end
+
 
   end
 
